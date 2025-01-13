1b2821af58c3ee2c39696f1a5e7ba7ea3a1b96e6
silviaralcaraz <silvia.rodriguez.alcaraz@rai.usc.es>
2025-01-13 11:43:56 +0000

    Deploying to gh-pages from @ silviaralcaraz/silviaralcaraz.github.io@c442e17097f9c1ecdaa0ad010d2306f346e2b245 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-i-class-fa-fa-home-i",
+    title: "<i class="fa fa-home"></i>",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
